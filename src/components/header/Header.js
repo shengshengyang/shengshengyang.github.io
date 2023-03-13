8c883f7c43cb6f7af4d4bebc23ffa738991185a4
deanyang122 <111415664+deanyang122@users.noreply.github.com>
2023-03-13 17:33:49 +0800

    Modify: 修正Header footer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Header.css'
 
 function Header() {
   return (
     <header className="header">
       <h1>My Website</h1>
       <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
+        <ul>
+          <li><Link to="/"><p>Home</p></Link></li>
+          <li> <Link to="about"><p>About</p></Link></li>
+          <li><Link to="sitemap"><p>Sitemap</p></Link> </li>
+        </ul>
+        
+       
         {/* You can also use relative links */}
         {/* For example, if you are on /about page, this will link to /about/sitemap */}
         {/* If you are on / page, this will link to /sitemap */}
         {/* To avoid confusion, it is better to use absolute links */}
-        <Link to="sitemap">Sitemap</Link> 
+        
       </nav>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -10,9 +10,12 @@ import {
   
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <StrictMode>
     <BrowserRouter>
     <App/>
     </BrowserRouter>
+  </StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
